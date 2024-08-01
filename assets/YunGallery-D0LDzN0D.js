e0f4e3a4faa3163da1bc3ea108c6012109d265bf
伊蕾娜 <106392719+lenfeng666@users.noreply.github.com>
2024-08-02 01:41:06 +0800

    修复公众号显示

---
+++ assets/YunGallery-D0LDzN0D.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-DdwuIiCE.js";import"./app-CI0qb0L-.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-CiHdsryW.js";export{o as default};
---
+++ assets/back-D1JV3elI.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-CI0qb0L-.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};

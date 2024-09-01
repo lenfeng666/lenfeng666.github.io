255d599a2263e1483eb12341ba0273a5d5be5476
伊蕾娜 <106392719+lenfeng666@users.noreply.github.com>
2024-09-01 23:56:40 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://fengyegf.cn/",
+  base: "/main",
   trailingSlash: "always",
   integrations: [
     tailwind(),
